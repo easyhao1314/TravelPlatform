1d7f2e066ffc1b6496aa4c8f8d55105b83aceca0
easyhao1314 <easyhao1314@126.com>
2013-06-19 23:54:42 +0800

    添加users的分页查询语句和datagrid的练习代码

--- PhoenixTravelAgencyPro/src/com/fenghuang/dao/impl/UsersDaoImpl.java
+++ PhoenixTravelAgencyPro/src/com/fenghuang/dao/impl/UsersDaoImpl.java
@@ -5,11 +5,13 @@ package com.fenghuang.dao.impl;
 
 import javax.sql.DataSource;
 
+import org.hibernate.validator.xml.PayloadType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.fenghuang.dao.BaseDao;
 import com.fenghuang.dao.IUserDao;
 import com.fenghuang.entiey.Users;
 import com.fenghuang.entiey.Usersrr;
@@ -27,7 +29,7 @@ import com.fenghuang.util.Pagination;
  * 
  */
 @Repository
-public class UsersDaoImpl extends JdbcTemplate implements IUserDao {
+public class UsersDaoImpl extends BaseDao implements IUserDao {
 
 	@Autowired
 	public UsersDaoImpl(@Qualifier("dataSource") DataSource dataSource) {
@@ -59,14 +61,14 @@ public class UsersDaoImpl extends JdbcTemplate implements IUserDao {
 				users.getDepartmentId(), users.getSortNumber(),
 				users.getAddress(), users.getZip(), users.getRemark(),
 				users.getImagePath(), users.getPassword());
-		return rs >0;
+		return rs > 0;
 	}
 
 	@Override
 	public boolean updateUsers(Users users) throws Exception {
-		
+
 		String sql = "update users set userNumber=?,userName=?,loginName=?,enName=?,sex=?,telephone=?,birthday=?,telephoneExt=?,email=?,mobilePhone=?,msn=?,fax=?,msn2=?,skype=?,msn3=?,qq=?,companyId=?,departmentId=?,jobDescription=?,sortNumber=?,address=?,zip=?,remark=?,imagePath=? where id =? ";
-        int rs = this.update(sql, users.getUserNumber(), users.getUserName(),
+		int rs = this.update(sql, users.getUserNumber(), users.getUserName(),
 				users.getLoginName(), users.getEnName(), users.getSex(),
 				users.getTelephone(), users.getBirthday(),
 				users.getTelephoneExt(), users.getEmail(),
@@ -75,30 +77,145 @@ public class UsersDaoImpl extends JdbcTemplate implements IUserDao {
 				users.getMsn3(), users.getQq(), users.getCompanyId(),
 				users.getDepartmentId(), users.getSortNumber(),
 				users.getAddress(), users.getZip(), users.getRemark(),
-				users.getImagePath(), users.getId());		
-		return rs >0;
+				users.getImagePath(), users.getId());
+		return rs > 0;
 	}
 
 	@Override
 	public boolean deleteUserById(Long id) throws Exception {
 		String sql = "delete from users where id=?";
 		int rs = this.update(sql, id);
-		return rs>0;
+		return rs > 0;
 	}
 
 	@Override
 	public Pagination<Users> getPaginationUsers(int currentPage,
 			int numPerPage, Users users) throws Exception {
 		StringBuffer sql = new StringBuffer("select * from users where 1=1 ");
-		
-		
-		return null;
+		if (users.getUserNumber() != null && !"".equals(users.getUserNumber())) {
+			sql.append("  and userNumber like '");
+			sql.append(users.getUserNumber());
+			sql.append("%'");
+		}
+
+		if (users.getUserName() != null && !"".equals(users.getUserName())) {
+			sql.append("  and userName like '");
+			sql.append(users.getUserName());
+			sql.append("%'");
+		}
+		if (users.getLoginName() != null && !"".equals(users.getLoginName())) {
+			sql.append("  and loginName like '");
+			sql.append(users.getLoginName());
+			sql.append("%'");
+		}
+
+		if (users.getEnName() != null && !"".equals(users.getEnName())) {
+			sql.append("  and enName like '");
+			sql.append(users.getEnName());
+			sql.append("%'");
+		}
+
+		if (users.getSex() != null && !"".equals(users.getSex())) {
+			sql.append("  and sex='");
+			sql.append(users.getSex());
+			sql.append("'");
+		}
+		if (users.getTelephone() != null && !"".equals(users.getTelephone())) {
+			sql.append("  and telephone like '");
+			sql.append(users.getTelephone());
+			sql.append("%'");
+		}
+		if (users.getTelephoneExt() != null
+				&& !"".equals(users.getTelephoneExt())) {
+			sql.append("  and telephoneExt like '");
+			sql.append(users.getTelephoneExt());
+			sql.append("%'");
+		}
+		if (users.getEmail() != null && !"".equals(users.getEmail())) {
+			sql.append("  and email like '");
+			sql.append(users.getEmail());
+			sql.append("%'");
+		}
+		if (users.getMobilePhone() != null
+				&& !"".equals(users.getMobilePhone())) {
+			sql.append("  and mobilePhone like '");
+			sql.append(users.getMobilePhone());
+			sql.append("%'");
+		}
+		if (users.getMsn() != null && !"".equals(users.getMsn())) {
+			sql.append("  and msn like '");
+			sql.append(users.getMsn());
+			sql.append("%'");
+		}
+		if (users.getFax() != null && !"".equals(users.getFax())) {
+			sql.append("  and fax like '");
+			sql.append(users.getFax());
+			sql.append("%'");
+		}
+		if (users.getMsn2() != null && !"".equals(users.getMsn2())) {
+			sql.append("  and msn2 like '");
+			sql.append(users.getMsn2());
+			sql.append("%'");
+		}
+		if (users.getSkype() != null && !"".equals(users.getSkype())) {
+			sql.append("  and skype like '");
+			sql.append(users.getSkype());
+			sql.append("%'");
+		}
+
+		if (users.getMsn3() != null && !"".equals(users.getMsn3())) {
+			sql.append("  and msn3 like '");
+			sql.append(users.getMsn3());
+			sql.append("%'");
+		}
+
+		if (users.getQq() != null && !"".equals(users.getQq())) {
+			sql.append("  and qq like '");
+			sql.append(users.getQq());
+			sql.append("%'");
+		}
+
+		if (users.getCompanyId() != null && users.getCompanyId() != 0) {
+			sql.append("  and companyId ='");
+			sql.append(users.getCompanyId());
+			sql.append("'");
+		}
+		if (users.getDepartmentId() != null && users.getDepartmentId() != 0) {
+			sql.append("  and departmentId ='");
+			sql.append(users.getDepartmentId());
+			sql.append("'");
+		}
+		if (users.getSortNumber() != null && users.getSortNumber() != 0) {
+			sql.append("  and sortNumber ='");
+			sql.append(users.getSortNumber());
+			sql.append("'");
+		}
+
+		if (users.getAddress() != null && !"".equals(users.getAddress())) {
+			sql.append("  and address like '");
+			sql.append(users.getAddress());
+			sql.append("%'");
+		}
+
+		if (users.getZip() != null && !"".equals(users.getZip())) {
+			sql.append("  and zip like '");
+			sql.append(users.getZip());
+			sql.append("%'");
+		}
+
+		if (users.getRemark() != null && !"".equals(users.getRemark())) {
+			sql.append("  and remark like '");
+			sql.append(users.getRemark());
+			sql.append("%'");
+		}
+		Pagination<Users>  paginUsers = this.getPagination(currentPage, numPerPage, sql.toString());
+		return paginUsers;
 	}
 
 	@Override
 	public Users getUsersById(Long id) throws Exception {
 		String sql = "select * from users where id=?";
-		Users user =this.queryForObject(sql, Users.class, id);
+		Users user = this.queryForObject(sql, Users.class, id);
 		return user;
 	}
 
