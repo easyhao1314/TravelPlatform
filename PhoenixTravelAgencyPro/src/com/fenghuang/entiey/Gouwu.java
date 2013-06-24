00316e6780ae0e83b8d1be051c53cb8e53ecfbca
wangdongqing <909482221@qq.com>
2013-06-24 17:00:26 +0800

    资源中心 王东清

---
+++ PhoenixTravelAgencyPro/src/com/fenghuang/entiey/Gouwu.java
@@ -0,0 +1,58 @@
+/***********************************************************************
+ * Module:  Gouwu.java
+ * Author:  Administrator
+ * Purpose: Defines the Class Gouwu
+ ***********************************************************************/
+package com.fenghuang.entiey;
+import java.util.*;
+
+/** 购物店
+ * 
+ * @pdOid 418e091c-6011-4d67-a833-30fe01dfeeb3 */
+public class Gouwu {
+   /** 编号
+    * 
+    * @pdOid 0c536be2-bd88-4eff-8c21-0786009d371f */
+   public long id;
+   /** 联系人
+    * 
+    * @pdOid 7361f899-7217-4639-908a-67abe2d9b7da */
+   public java.lang.String lianxiren;
+   /** 购物店名称
+    * 
+    * @pdOid c109fee7-60bf-4778-9da9-715b5b0a50a4 */
+   public java.lang.String name;
+   /** 传真
+    * 
+    * @pdOid 1a2b6339-42e6-4cb4-bfc7-d1fd370a1bcb */
+   public java.lang.String chuanzhen;
+   /** 城市
+    * 
+    * @pdOid 1d4b8a48-a4e7-4d3c-a632-7afa37984d73 */
+   public int chengshiId;
+   /** 地址
+    * 
+    * @pdOid b632aace-6ccd-4d19-80c9-36d8fed32dcf */
+   public java.lang.String dizhi;
+   /** 电话
+    * 
+    * @pdOid 04aa7bcf-bfeb-4cfa-9aaa-1e8c90ce4790 */
+   public java.lang.String dianhua;
+   /** 手机
+    * 
+    * @pdOid 1e21d3d6-b878-4f10-825a-cd2403c31c55 */
+   public java.lang.String shouji;
+   /** 邮箱
+    * 
+    * @pdOid a8439cd5-2769-4cca-a844-475b7f39864a */
+   public java.lang.String email;
+   /** 合作级别
+    * 
+    * @pdOid b04cf223-429f-4e84-b1d0-b748681dfc63 */
+   public int hzjbId;
+   /** 备注
+    * 
+    * @pdOid 8f5c6e7c-18db-4bf8-bc11-8d169fa4c91a */
+   public java.lang.String bz;
+
+}
