b71db47486599081f412431fee87e3a16e182807
easyhao1314 <easyhao1314@126.com>
2013-08-25 17:05:24 +0800

    常量工具类，添加提交参数

--- PhoenixTravelAgencyPro/src/com/fenghuang/util/ShowConstant.java
+++ PhoenixTravelAgencyPro/src/com/fenghuang/util/ShowConstant.java
@@ -19,5 +19,10 @@ public class ShowConstant {
 	public static final String MENU_TYPE_MAINMENU = "1";
 	
 	public static final String MENU_TYPE_SUBMENU = "2";
+	
+    public static final String SUBMIT_TYPE = "submittype";
+	public static final String SUBMIT_DANTUAN_OPERATE="dantuanshenp";
+	
+	public static final String SUBMIT_CAIWU_OPERATE="CAIWU";
 
 }
